a5d4dfd694427e68a60a4547ad54714c220972ab
doblel <lucattilisandro@gmail.com>
2017-02-03 17:53:55 -0300

    [FIX] example

--- example.py
+++ example.py
@@ -1,25 +1,27 @@
+"""example.py."""
 from flask import Flask
 from flask_hooker.hooker import Hooker
 
 
+def print_username(json):
+    """Print name."""
+    print 'username: %s' % json['username']
 
-def github_push(json):
-    print json
 
 app = Flask(__name__)
 
 hooker = Hooker(app=app, url_prefix='/webhook')
 
-hooker.add_handler(event='push', func=github_push, event_type='X-Github-Event')
+hooker.add_handler(event='name', func=print_username, event_type='X-Custom-Event')
 
 
 @app.route('/')
 def index():
+    """Index view of your app."""
     return '<h> Flask-Hooker </>'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run(debug=True, host='138.0.184.1', port=3000)
 
 # $curl -H "Content-Type:application/json" -H "X-Custom-Event:name" -d '{"username":"doblel"}' -X POST localhost:5000/webhook
